7588c58ed9d785509c284a2a94fc4a0f7fd08560
FriendlyUsername <leonmein@gmx.net>
2023-04-18 12:08:03 +0200

    loading stages

--- app/_components/layout-helper.tsx
+++ app/_components/layout-helper.tsx
@@ -1,23 +1,4 @@
-import React, { Suspense } from "react"
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs/app-beta"
-import { MySignInButton } from "./signin"
-import Link from "next/link"
-
-// const AsyncVodsButton = async ({ slug }: { slug: string | undefined }) => {
-//   const user = await currentUser();
-
-//   if (user?.username === slug) return null;
-
-//   return <GoToVodsButton user={user?.username} />;
-// };
-
-const TopRightNav = async ({ slug }: { slug: string | undefined }) => {
-  return (
-    <>
-      <div className="flex h-12 w-12 items-center"></div>
-    </>
-  )
-}
+import React from "react"
 
 export const LayoutHelper = async ({
   children,
